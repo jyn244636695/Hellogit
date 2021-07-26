4bcb409e6e8ebbc5532dd2223342f4906400df3e
jyn <244636695@qq.com>
2021-07-26 14:57:27 +0800

    2尝试git

--- src/main/java/Hellogit.java
+++ src/main/java/Hellogit.java
@@ -2,5 +2,6 @@ public class Hellogit {
     public static void main(String[] args) {
         System.out.println("hellogit");
         System.out.println("失败了");
+        System.out.println("成功");
     }
 }
